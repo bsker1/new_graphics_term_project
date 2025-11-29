f46e8c3949ba65225470878ea570510f48ce57c9
bsker1 <kiserb17@gmail.com>
2025-11-29 16:50:29 -0500

    Initial commit, create window with OpenGL context using GLFW and GLAD

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
+
+int main(void) {
+  GLFWwindow* window;
+
+  /* Initialize the library */
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  gladLoadGL();
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
