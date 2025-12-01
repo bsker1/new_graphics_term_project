83eba4ad941da77360836fe583f039528ea3b2ff
bsker1 <kiserb17@gmail.com>
2025-12-01 13:18:06 -0500

    Add necessary glm headers to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "glad/glad.h"
 #include "glfw/glfw3.h"
 #include "stb/stb_image.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 #include <cmath>
 #include <iostream>
